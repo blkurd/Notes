f6d93c8fd1e330dc78e69e92bfb6594aeb0ea8c7
blkurd <blkurd@hotmail.com>
2023-01-25 14:41:40 -0500

    Added Schema for notes

---
+++ models/notes.js
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////
+//// Our schema and model for the fruit resource          ////
+//////////////////////////////////////////////////////////////
+const mongoose = require('mongoose') // import mongoose
+const { stringify } = require('querystring')
+
+// we'll destructure the Schema and model functions from mongoose
+const { Schema, model } = mongoose
+
+// notes Schema
+const notesSchema = new Schema ({
+        title: string,
+        text : string,
+        color : string
+})
+
+// make the notes model
+// the model method takes two argumetns 
+// the first is what we call our model 
+// the second is the schema used to build the model
+
+const Note = model ('Note', notesSchema )
+
+
+//////////////////////////////////////////////////////////////
+//// Export model          ////
+//////////////////////////////////////////////////////////////
+
+module.exports = Note
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express') // import the express framework
 const mongoose = require('mongoose') // import the mongoose library
 const morgan = require('morgan') // import the morgan request logger
 require('dotenv').config() // Load my ENV file's variables
+const path = require('path') // import path module
 
 /////////////////////////////////////
 //// Database Connection         ////
@@ -32,6 +33,78 @@ mongoose.connection
 //// Create our Express App Object //
 /////////////////////////////////////
 const app = express()
+
+
+/////////////////////////////////////
+//// Middleware                     ////
+/////////////////////////////////////
+
+// middleware runs before all the routes.
+// every request is processed through our middleware before mongoose can do anything with it
+app.use(morgan('tiny')) // this is for request loggging, the 'tiny' argument declares what size of morgan log to use
+// example : GET / 304 - - 7.108 ms
+app.use(express.urlencoded({ extended: true })) //this parses urlEncoded request bodies(useful for POST and PUT requests)
+app.use(express.static('public')) // this serves static files from the 'public' folder
+app.use(express.json()) // parses incoming request payloads with JSON
+// So when we start sending data to our server to create items in our database.
+// This piece of middleware is going to be able to parse those Json payloads and turn them into something that our app can use.
+
+
+
+/////////////////////////////////////
+//// Routes                      ////
+/////////////////////////////////////
+app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+})
+
+// we're going to build a seed route
+// this will seed the database for us with a few starter resources
+// There are two ways we will talk about seeding the database
+// Note -> seed route, they work but they are not best practices
+// Second -> seed script, they work and they ARE best practices
+app.get('/notes/seed', (req, res) => {
+    // array of starter resources(Notes)
+    const startNotes = [
+        { title: '', text: '',color: 'yellow'},
+        { title: '', text: '',color: 'purple'},
+        { title: '', text: '',color: 'green'},
+        { title: '', text: '',color: 'red'},
+        { title: '', text: '',color: 'brown'}
+    ]
+    // then we delete every note in the database(all instances of this resource)
+    Note.deleteMany({})
+        .then(() => {
+            // then we'll seed(create) our starter notes
+            Note.create(startNotes)
+                // tell our db what to do with success and failures
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('The following error occurred: \n', err))
+        })
+})
+
+// index route -> displays all notes
+app.get('/notes', (req, res) => {
+    // find all the notes
+    Note.find({})
+        // send json if successful
+        .then(notes => { res.json({notes:notes })})
+        // catch errors if they occur
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+
+
+
+
+
+
+
+
+
+
 
 /////////////////////////////////////
 //// Server Listener             ////

c2f35ff31109cd8331827590b87f008adaee2459
blkurd <blkurd@hotmail.com>
2023-01-27 23:40:04 -0500

    Added Authorization

--- controllers/noteControllers.js
+++ controllers/noteControllers.js
@@ -18,6 +18,11 @@ const router = express.Router();
 router.get("/", (req, res) => {
   // find all the notes
   Note.find({})
+    // there's a built in function that runs before the rest of the promise chain
+   // this function is called populate, and it's able to retrieve info 
+  //  from other documents in other collections
+        .populate('owner', 'username')
+        // .populate('comments.author', '-password')
     // send json if successful
     .then((notes) => {
       res.json({ notes: notes });
@@ -36,6 +41,10 @@ router.post("/", (req, res) => {
   // here, we'll have something called a request body
   // inside this function, that will be called req.body
   // we want to pass our req.body to the create method
+  // we want to add an owner field to our note
+  // It is great that we saved the user's id on the session object
+  // so it is easy for us to access that data.
+  req.body.owner = req.session.userId;
   const newNote = req.body;
   Note.create(newNote)
     // send a 201 status, along with the json response of the new note
@@ -50,51 +59,127 @@ router.post("/", (req, res) => {
     });
 });
 
+// GET route
+// Index -> This is a user specific index route
+// this will only show the logged in user's notes
+
+router.get('/mine', (req, res) => {
+  // find notes by ownership, using the req.session info
+  Note.find({ owner: req.session.userId })
+      .populate('owner', 'username')
+      // .populate('comments.author', '-password')
+      .then(notes => {
+          // if found, display the notes
+          res.status(200).json({ notes: notes })
+      })
+      .catch(err => {
+          // otherwise throw an error
+          console.log(err)
+          res.status(400).json(err)
+      })
+})
+
+
+
+
+
 // PUT route
 // Update -> updates a specific note
 // PUT replaces the entire document with a new document from the req.body
 // PATCH is able to update specific fields at specific times, but it requires a little more code to ensure that it works properly, so we'll use that later
-router.put("/:id", (req, res) => {
-  // save the id to a variable for easy use later
-  const id = req.params.id;
-  // save the request body to a variable for easy reference later
-  const updatedNote = req.body;
-  // we're going to use the mongoose method:
-  // findByIdAndUpdate
-  // eventually we'll change how this route works, but for now,
-  // we'll do everything in one shot, with findByIdAndUpdate
-  Note.findByIdAndUpdate(id, updatedNote, { new: true })
-    .then((note) => {
-      console.log("the newly updated note", note);
-      // update success message will just be a 204 - no content
-      res.sendStatus(204);
-    })
-    // .catch((err) => console.log(err));
-    .catch((err) => {
-      console.log(err);
-      res.status(404).json(err);
-    });
-});
+// router.put("/:id", (req, res) => {
+//   // save the id to a variable for easy use later
+//   const id = req.params.id;
+//   // save the request body to a variable for easy reference later
+//   const updatedNote = req.body;
+//   // we're going to use the mongoose method:
+//   // findByIdAndUpdate
+//   // eventually we'll change how this route works, but for now,
+//   // we'll do everything in one shot, with findByIdAndUpdate
+//   Note.findByIdAndUpdate(id, updatedNote, { new: true })
+//     .then((note) => {
+//       console.log("the newly updated note", note);
+//       // update success message will just be a 204 - no content
+//       res.sendStatus(204);
+//     })
+//     // .catch((err) => console.log(err));
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(404).json(err);
+//     });
+// });
+
+// PUT route
+// Update -> updates a specific note(only if the note's owner is updating)
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  Note.findById(id)
+      .then(note => {
+          // if the owner of the note is the person who is logged in
+          if (note.owner == req.session.userId) {
+              // send success message
+              res.sendStatus(204)
+              // update and save the note
+              return note.updateOne(req.body)
+          } else {
+              // otherwise send a 401 unauthorized status
+              res.sendStatus(401)
+          }
+      })
+      .catch(err => {
+          console.log(err)
+          res.status(400).json(err)
+      })
+})
+
+
 
 // DELETE route
 // Delete -> delete a specific note
 
-router.delete("/:id", (req, res) => {
-  // get the id from the req
-  const id = req.params.id;
-  // find and delete the note
-  Note.findByIdAndRemove(id)
-    // send a 204 if successful
-    .then(() => {
-      res.sendStatus(204);
-    })
-    // send an error if not
-    // .catch((err) => console.log(err));
-    .catch((err) => {
-      console.log(err);
-      res.status(404).json(err);
-    });
-});
+// router.delete("/:id", (req, res) => {
+//   // get the id from the req
+//   const id = req.params.id;
+//   // find and delete the note
+//   Note.findByIdAndRemove(id)
+//     // send a 204 if successful
+//     .then(() => {
+//       res.sendStatus(204);
+//     })
+//     // send an error if not
+//     // .catch((err) => console.log(err));
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(404).json(err);
+//     });
+// });
+
+
+// DELETE route
+// Delete -> delete a specific note
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  Note.findById(id)
+      .then(note => {
+          // if the owner of the note is the person who is logged in
+          if (note.owner == req.session.userId) {
+              // send success message
+              res.sendStatus(204)
+              // delete the note
+              return note.deleteOne()
+          } else {
+              // otherwise send a 401 unauthorized status
+              res.sendStatus(401)
+          }
+      })
+      .catch(err => {
+          console.log(err)
+          res.status(400).json(err)
+      })
+})
+
+
+
 
 // SHOW route
 // Read -> finds and displays a single resource
--- models/notes.js
+++ models/notes.js
@@ -1,36 +1,52 @@
 //////////////////////////////////////////////////////////////
 //// Our schema and model for the note resource          ////
 //////////////////////////////////////////////////////////////
-// This is the old mongoose import and we want the mongoose object to relate to our db 
+// This is the old mongoose import and we want the mongoose object to relate to our db
 // so we are going to bring in the mongoose connection from our utils
 // const mongoose = require('mongoose') // import mongoose
-const mongoose = require('../utils/connection') //import mongoose from utils
-const { stringify } = require('querystring')
+const mongoose = require("../utils/connection"); //import mongoose from utils
+// const { stringify } = require('querystring')
 
 // we'll destructure the Schema and model functions from mongoose
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 // notes Schema
-const notesSchema = new Schema ({
-        owner: String,
-        title: String,
-        text : String,
-        color : String
-})
+const notesSchema = new Schema(
+  {
+    owner: {
+      // this is where we set up an objectId reference
+      // by declaring that as the type
+      type: Schema.Types.ObjectId,
+      // this line tells us which model to look at
+      ref: "User",
+    },
+
+    title: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
 
 // make the notes model
-// the model method takes two argumetns 
-// the first is what we call our model 
+// the model method takes two argumetns
+// the first is what we call our model
 // the second is the schema used to build the model
 // The mongoose connection is attached to our notes model so we don't
 // even need the model because the Controller file talks to the model
 // file which talks to the connection file and the controller file talks
 // to server.js
-const Note = model ('Note', notesSchema )
-
+const Note = model("Note", notesSchema);
 
 //////////////////////////////////////////////////////////////
 //// Export model          ////
 //////////////////////////////////////////////////////////////
 
-module.exports = Note
+module.exports = Note;
